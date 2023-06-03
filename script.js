5964e28b9e58efce5fc97ed0b1b83d148fc2fdf7
xadvent <piercestrutt@gmail.com>
2023-06-03 17:03:04 -0600

    Menu for coloring choice of colors after drawn I could not figure out how to change the stylesheet using JS as it seems that access is restricted for some reason. Will be fixing that next.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const container = document.querySelector('.content')
 const columnDiv = document.querySelector('.columnDiv')
+const slider = document.getElementById("myRange");
+const output = document.querySelector(".slider");
+output.innerHTML = slider.value; // Display the default slider value
+const sliderDescription = document.createElement('div')
+
+sliderDescription.classList.add('sliderDescription')
+const sliderMainContainer = document.querySelector('.slideContent');
 
 const getGrid = function(size) {
 for (x=0; x< size; x++){
@@ -28,21 +35,57 @@ const addDrawing = function(width, startingRowDiv) {
     }}
 addDrawing(width,startingRowDiv);
 
-const slider = document.getElementById("myRange");
-const output = document.querySelector(".slider");
-output.innerHTML = slider.value; // Display the default slider value
-const sliderDescription = document.createElement('div')
-
-sliderDescription.classList.add('sliderDescription')
-const sliderMainContainer = document.querySelector('.slideContent');
 
 slider.oninput = function() {
     document.querySelectorAll('.rowDiv').forEach(e => e.remove());
-    document.querySelectorAll('.columnDiv').forEach(e => e.remove());
+    document.querySelectorAll('.columnDiv').forEach(e => e.remove())
 
     console.log(output.innerHTML = this.value);
     output.innerHTML = this.value;
     getGrid(output.innerHTML = this.value);
     let changedRowDiv = document.querySelectorAll('.rowDiv');
     addDrawing(changedRowDiv.length, changedRowDiv)
+}
+
+const removeColor = function () {
+    box = document.querySelectorAll('.rowDiv')
+    box.forEach(box => box.classList.remove('color'))
+}
+
+
+const colorChanger = function (color) {
+    //const fuck = document.styleSheets[0].cssRules
+    //const rules = stylesheet.cssRules[1]
+    //let elementRules = null;
+    //console.log(fuck)
+    //for (let i = 0; i< StyleSheetList[0].length; i++) {
+        //if (stylesheet.CSSRules[i].selectorText === 'color') {
+        //    elementRules = stylesheet.cssRule[i];
+       // }
+    //}
+    //elementRules.style.setProperty('background', color)
+    //console.log(elementRules)'
+
+    previous = document.querySelectorAll('.color')
+    for (i of previous){
+        i.style.backgroundColor = color;
+    }
+}
+
+
+const colorList = document.getElementById('colorList').getElementsByTagName('button')
+for (item of colorList) {
+    item.classList.add('colorChoice')
+}
+
+const colorButton = document.querySelectorAll('.colorChoice')
+for (i = 0; i < colorButton.length; i++) {
+    colorButton[i].addEventListener('click', function() {
+        const textContent = this.textContent
+        const prev = document.querySelectorAll('.color')
+        for (single of prev) {
+            (single != 'Rainbow')
+            single.style.backgroundColor = textContent
+        }
+    })
 }
