ce23059eae40b515724beee6cde5b3d96f3f65fa
xadvent <piercestrutt@gmail.com>
2023-06-03 13:17:40 -0600

    Added slider to change amount of squares

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
 const container = document.querySelector('.content')
 const columnDiv = document.querySelector('.columnDiv')
 
-
-const getGrid = function() {
-// fill in with for loops below using a prompt to get the loop amount value.
-}
-for (x=0; x< 32; x++){
+const getGrid = function(size) {
+for (x=0; x< size; x++){
     const boxCreate = document.createElement('div')
     boxCreate.classList.add('columnDiv')
     container.appendChild(boxCreate)   
 
-    for (i=0; i< 32; i++){
+    for (i=0; i< size; i++){
         const otherDiv = document.createElement('div')
         otherDiv.classList.add('rowDiv')
         boxCreate.appendChild(otherDiv)
     }
+}}
+getGrid(32)
+
+const startingRowDiv = document.querySelectorAll('.rowDiv')
+const width = startingRowDiv.length
+
+const addDrawing = function(width, startingRowDiv) {
+    for (let i=0; i < width; i++){
+        startingRowDiv[i].addEventListener("mouseover",function(i){
+            console.log(i)
+            let classOf = this.classList; 
+            classOf.add('color')
+        });
+    }}
+addDrawing(width,startingRowDiv);
+
+const slider = document.getElementById("myRange");
+const output = document.querySelector(".slider");
+output.innerHTML = slider.value; // Display the default slider value
+const sliderDescription = document.createElement('div')
+
+sliderDescription.classList.add('sliderDescription')
+const sliderMainContainer = document.querySelector('.slideContent');
+
+slider.oninput = function() {
+    document.querySelectorAll('.rowDiv').forEach(e => e.remove());
+    document.querySelectorAll('.columnDiv').forEach(e => e.remove());
+
+    console.log(output.innerHTML = this.value);
+    output.innerHTML = this.value;
+    getGrid(output.innerHTML = this.value);
+    let changedRowDiv = document.querySelectorAll('.rowDiv');
+    addDrawing(changedRowDiv.length, changedRowDiv)
 }
-
-const totalRowDiv = document.querySelectorAll('.rowDiv')
-for (let i=0; i<totalRowDiv.length; i++){
-    console.log(totalRowDiv[i])
-
-    totalRowDiv[i].addEventListener("mouseover",function(i){
-        console.log(i)
-        let classOf = this.classList; 
-        classOf.add('color')
-    });
-}
-
-const addColor = function () {
-
-}
-    
-    
-
-
